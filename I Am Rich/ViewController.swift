b519e11ccca7dfbbb11cf30131d03b783f9647c8
ellaisa <ellaisa@bk.ru>
2020-11-05 14:35:01 +0300

    my first project from Udemy

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Эллаиса Даценко on 14.08.2020.
+//  Copyright © 2020 Эллаиса Даценко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
